25f3ee62e0d909ac8f9eb0eb13b35d2d369fb922
Marten/Qqwy <w-m@wmcode.nl>
2021-10-04 15:35:35 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Extractable.Mixfile do
   def project do
     [
       app: :extractable,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
