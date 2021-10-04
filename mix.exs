54c35598029c23811bb1986ac3e5c9eec5385d0c
Marten/Qqwy <w-m@wmcode.nl>
2021-10-04 12:27:38 +0200

    Drop elixir 1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Extractable.Mixfile do
   def project do
     [app: :extractable,
      version: "1.0.0",
-     elixir: "~> 1.7",
+     elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
